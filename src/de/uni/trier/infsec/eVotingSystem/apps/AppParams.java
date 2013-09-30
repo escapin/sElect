f7b0544910409276284b4289e99ffed9a5d4c028
Enrico Scapin <scapin@uni-trier.de>
2013-09-30 16:44:01 +0200

    add candidate names to AppParams

--- src/de/uni/trier/infsec/eVotingSystem/apps/AppParams.java
+++ src/de/uni/trier/infsec/eVotingSystem/apps/AppParams.java
@@ -5,7 +5,23 @@ import java.io.File;
 public class AppParams 
 {
 	public static final String PATH_STORAGE = System.getProperty("java.io.tmpdir") + File.separator + "eVotingSystem" + File.separator;
-	public static final byte[] electionID = "Election Nr 123".getBytes();
+	
+	public static final String APPNAME = "eVoteTrust - RS3 Demo";
+	
+	public static final String ELECTIONMSG = "Who is your favourite candidate?";
+	public static final byte[] ELECTIONID = "ElectionTest Nr 153".getBytes();
+	public static final String[] CANDIDATESARRAY = {	
+								"Candidate 00",
+								"Candidate 01",
+								"Candidate 02",
+								"Candidate 03",
+								"Candidate 04",
+								"Candidate 05",
+								"Candidate 06",
+								"Candidate 07",
+								"Candidate 08",
+								"Candidate 09",
+								};
 
 	public static String SERVER1_NAME = "localhost";
 	public static int SERVER1_PORT= 7075;
