5d42089cea6bc3613fd6da3a065a31698597146c
Enrico Scapin <scapin@uni-trier.de>
2014-09-25 11:40:16 +0200

    storage paths updated

--- src/de/uni/trier/infsec/eVotingSystem/apps/AppParams.java
+++ src/de/uni/trier/infsec/eVotingSystem/apps/AppParams.java
@@ -6,13 +6,17 @@ import de.uni.trier.infsec.eVotingSystem.bean.URI;
 
 public class AppParams 
 {
-	public static final String PATH_STORAGE = System.getProperty("java.io.tmpdir") + File.separator + "eVotingSystem" + File.separator;
-	public static final String RECEIPT_file = PATH_STORAGE + "receipt_"; // + voterID + ".msg";
+	public static final String PATH_STORAGE = System.getProperty("user.home") + File.separator + ".eVotingSystem" + File.separator;
 	
-	public static final String PUBLIC_KEY_path = PATH_STORAGE + "PublicKeys" + File.separator;
-	public static final String PRIVATE_KEY_path = PATH_STORAGE + "PrivateKeys" + File.separator;
+	public static final String PUBLIC = PATH_STORAGE + "Public" + File.separator;
+	public static final String PUBLIC_KEY_path = PUBLIC + "PublicKeys" + File.separator;
+	public static final String EL_MANIFEST_path = PUBLIC + "Manifest" + File.separator;
 	
-	public static final String PKI_DATABASE = System.getProperty("java.io.tmpdir") + File.separator + "PKI_server.db";
+	public static final String PRIVATE = PATH_STORAGE + "Private" + File.separator;
+	public static final String PRIVATE_KEY_path = PRIVATE + "PrivateKeys" + File.separator;
+	public static final String RECEIPT_file = PRIVATE + "Receipts" + "receipt_"; // + voterID + ".msg";
+	
+	//public static final String PKI_DATABASE = System.getProperty("java.io.tmpdir") + File.separator + "PKI_server.db";
 	// = de.uni.trier.infsec.functionalities.pki.PKIServerCore.DEFAULT_DATABASE;
 	
 	// RESULT PATH (by assuming that the execution of the servers is from the bin/ directory 
@@ -22,14 +26,14 @@ public class AppParams
 	//public static final String FIN_SERVER_RESULT_file = PATH_STORAGE + "SignedFinalResult.msg";
 	public static final String FINAL_RESULT_file = "../BulletinBoard/FinalResult.txt";
 	
-	public static final String EL_MANIFEST_path = PATH_STORAGE + "Manifest" + File.separator;
+	
 	
 	/*
 	 * In case we store local file in memory, remember to add the path also here
 	 * in such a way that we keep track of them it in unique array.
 	 * (For instance, it is useful for deleting them).
 	 */
-	public static final String[] PATH_LOCALFILES = 	{	PKI_DATABASE, 
+	public static final String[] PATH_LOCALFILES = 	{	// PKI_DATABASE, 
 														COLL_SERVER_RESULT_msg,
 														FIN_SERVER_RESULT_msg,
 														FINAL_RESULT_file
@@ -42,8 +46,8 @@ public class AppParams
 	public static final String VERIFYAPPNAME = "VerifYourVote";
 	
 	
-	public static final long STARTTIME = System.currentTimeMillis() + (60000); //*10;
-	public static final long DURATION = (60000)*3; 
+	public static final long STARTTIME = System.currentTimeMillis() + (60000); // election starts in 1 minute
+	public static final long DURATION = (60000)*3; // 3 minutes
 	
 	public static final String EL_TITLE = "Favourite RS3 project";
 	public static final String EL_DESCRIPTION = "Election for the best RS3 project";
