2de51325bc04d88a6793454df29b8cad1806267b
Tomasz Truderung <ttruderung@gmail.com>
2014-11-20 15:39:13 +0100

    voting both: polish user interface

--- VotingBooth/js/selectBooth.js
+++ VotingBooth/js/selectBooth.js
@@ -1,11 +1,13 @@
 
 function selectBooth() {
 
-    // PARAMETERS
+    //////////////////////////////////////////////////////////////////////////////
+    /// PARAMETERS
 
     var FADE_TIME = 260;
 
-    // STATE
+    //////////////////////////////////////////////////////////////////////////////
+    /// STATE
 
     // For navigation
     var activeTabId = "#welcome";
@@ -16,12 +18,13 @@ function selectBooth() {
     var choice = null;
 
 
-    // FUNCTIONS AND HANDLERS
-    
+    //////////////////////////////////////////////////////////////////////////////
+    /// HANDLERS FOR SUMBITTING DATA 
+
     function onSubmitWelcome(event) 
     {
         // Consuming the input
-        var e = $('#inp-email').val()
+        var e = $('#inp-email').val();
         if( !e || e==='' ) {
             console.log('Undefined or empty email');
         }
@@ -54,7 +57,7 @@ function selectBooth() {
     function onSubmitChoice(event) {
         // make the active tab disappear
         $(activeTabId).fadeOut(FADE_TIME, function() {
-            activeTabId = '#result';
+            activeTabId = '#error';
 
             // make the otp tab appear
             $(activeTabId).fadeIn(FADE_TIME);
@@ -76,12 +79,47 @@ function selectBooth() {
         return false; // prevents any further submit action
     }
 
+    //////////////////////////////////////////////////////////////////////////////
+    /// OTHER HANDLERS 
 
-    // INITIALISATION AND BINDING
+    function enableWhenNotEmpty(button, input) {
+        return function() {
+            var v = input.val();
+            if( v==='' ) 
+                button.prop('disabled', true);
+            else 
+                button.prop('disabled', null);
+        };
+    }
+
+    function whenChoiceChanges() {
+        // The selected item should be displayed in a stronger way. So:
+        // Reset 'checked' class and add notchecked class for all the labels in the form
+        $("#choice-form label").addClass('notchecked').removeClass('checked');
+        // And do the oposite for the selected one
+        $("#choice-form label[for='" + this.id + "']").addClass('checked').removeClass('notchecked');
+
+        // Update the status of the submit button (active only when something is selected)
+        if($('input[name="choice"]:checked').length == 0) { // no choice selected 
+            $('#submit-choice').prop('disabled', true);
+            console.log('disabling');
+        }
+        else { // some choice selected
+            $('#submit-choice').prop('disabled', null);
+            console.log('enabling');
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// INITIALISATION AND BINDING
+    
     $('#welcome form').submit(onSubmitWelcome);
     $('#otp form').submit(onSubmitOTP);
     $('#choice form').submit(onSubmitChoice);
     $('#error form').submit(onSubmitError);
+    $('#inp-email').on('input', enableWhenNotEmpty($('#submit-email'), $('#inp-email')));
+    $('#inp-otp').on('input', enableWhenNotEmpty($('#submit-otp'), $('#inp-otp')));
+    $('input[name="choice"]').change(whenChoiceChanges);
     
     // Focus on the email input
     $('#inp-email').focus();
