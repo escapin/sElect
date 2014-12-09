0ebba145ad834d290487316d0de1a76f67dac36d
Tomasz Truderung <ttruderung@gmail.com>
2014-12-09 21:47:12 +0100

    add a tool for key generation

---
+++ tools/keyGen.js
@@ -0,0 +1,18 @@
+var crypto = require('cryptofunc');
+
+var ek = crypto.rsa_keygen();
+
+// console.log(enckeys);
+
+var sk = crypto.sig_keygen();
+
+// console.log(sigkeys);
+
+var keys = { encryptionKey: ek.encryptionKey, 
+             decryptionKey: ek.decryptionKey,
+             verificationKey: sk.verificationKey,
+             signingKey: sk.signingKey
+             }
+
+console.log(keys);
+
