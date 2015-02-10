88d44d6078c21676f33e3d4ef47fb8493722f638
escapin <me@escapin.net>
2015-02-10 14:15:03 +0100

    innerBallots sent right away to the next mix server

--- MixServer/src/routes.js
+++ MixServer/src/routes.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var request = require('request-json');
 var config = require('../config');
 var manifest = require('./manifest');
 var server = require('./server');
@@ -7,33 +8,77 @@ var resultReady = false;
 if (fs.existsSync(config.RESULT_FILE))
     resultReady = true;
 
+function saveResult(innerBallots) {
+	fs.writeFile(config.RESULT_FILE, innerBallots, function (err) {
+        if (err) 
+            console.log('Problems with saving result', config.RESULT_FILE);
+        else {
+            console.log('Result saved in', config.RESULT_FILE);
+            resultReady = true;
+        }
+    });
+}
+
 // Processes the ballots 
-// Parameter res is optional. If present, the summary
+// Parameter 'res' is optional. If present, the summary
 // (success/failure) will be sent to it.
+// Function 'sendResult' is optional. If present, it will
+// send the innerBallots to the next server.
 //
-function processBallots(data, res) {
+function processBallots(data, res, sendResult) {
     server.processBallots(data, function (err, result) {
         if (err) {
             console.log(' ...INTERNAL ERROR. Cannot process the ballots: ', err);
             console.log('RESULT NOT SAVED');
             if (res) res.send({ ok: false, info: 'INTERNAL ERROR' });
         }
-        else if (result.ok) {
-            var innerBallots = result.data;
-            // Save the inner ballots:
-            fs.writeFileSync(config.RESULT_FILE, innerBallots);
-            console.log('Result saved in', config.RESULT_FILE);
-            resultReady = true;
-            if (res) res.send({ ok: true, info: 'Data accepted'});
-        }
-        else {
+        else if (!result.ok) {
             console.log('ERROR:', result.data);
             console.log('RESULT NOT SAVED');
             if (res) res.send({ ok: false, info: result.data });
         }
+        else {
+            var innerBallots = result.data;
+            // save the  inner ballots
+            saveResult(innerBallots);    
+            if (res) res.send({ ok: true, info: 'Data accepted'});
+            // send the inner ballots to the next server, if it exists
+            if (sendResult) sendResult(innerBallots);
+        }
     });
 }
 
+var mixserv_options = {};
+if (config.ignore_fin_serv_cert)
+    mixserv_options = {rejectUnauthorized: false};
+
+// Send result to the next mix server, if it is listed in the manifest.
+// Print only a message error, otherwise.
+function sendResultToNextMix(innerBallots) {
+	var index 	= config.chainIndex;
+	if(index+1 >= +manifest.mixServers.length) {
+		console.log('Warning: result not sent. No other mix servers.');
+	}
+	else {
+		console.log('Sending result to the next mix server (#%d)', index+1);
+		var nextMixServ = request.newClient(manifest.nextMixServers[index+1].URI, mixserv_options);
+		var data = {data: innerBallots}
+		// one could add something like {timeout:10000} to the request below, after 'data'
+		nextMixServ.post('data', data, function(err, res, body) {
+			if (err) {
+				console.log(' ...Error: Cannot send the result to the next mix server: ', err);
+			}
+			else {
+				console.log(' ...Result sent to the next mix server (#%d).', index+1);
+				console.log(' ...Response:', body);
+			}
+		});
+	}
+}
+
+
+// Get the data (in 'req'), processes it as the ballots, 
+// and send it to the next mix server
 exports.process = function process(req, res) 
 {
     if (resultReady) {
@@ -43,8 +88,20 @@ exports.process = function process(req, res)
     }
     var data = req.body.data;
     console.log('Ballots coming. Processing...');
-    processBallots(data, res);
-};
+    // process the ballots and ...
+    if(config.chainIndex+1 >= +manifest.mixServers.length) // no other mix servers
+    	processBallots(data, res);
+    else // ...and send the result to the next mix server
+    	processBallots(data, res, sendResultToNextMix);
+}
+
+// Reads data from the given file and  processes it as the ballots
+exports.processFile = function processFile(dataFileName) {
+    console.log('Processing the file ', dataFileName);
+    var data = fs.readFileSync(dataFileName, {encoding:'utf8'});
+    processBallots(data);
+}
+
 
 exports.statusPage = function statusPage(req, res) {
     var status = resultReady ? 'result ready' : 'waiting for data';
@@ -64,11 +121,4 @@ exports.serveFile = function serveFile(path) {
             }
         });
     }
-}
-
-// Reads data from the given file and  processes it as the ballots
-exports.processFile = function processFile(dataFileName) {
-    console.log('Processing the file ', dataFileName);
-    var data = fs.readFileSync(dataFileName, {encoding:'utf8'});
-    processBallots(data);
 }
