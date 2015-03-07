5f23502d3e231ab2de9550735fbafaec7c6113fa
escapin <me@escapin.net>
2015-03-07 10:43:01 +0100

    make build before generating the crypto keys

--- templates/updateMixServersKeys.py
+++ templates/updateMixServersKeys.py
@@ -2,21 +2,29 @@ import os
 import re
 import subprocess
 
-path = "../templates/"
 pattern="config_mix[0-9]+.json"
 electionManifest="ElectionManifest.json"
-keyGenerator_file="../tools/genKeys4mixServer.js"
+tools_path = "../tools/"
+keyGenerator_file="genKeys4mixServer.js"
+current = os.getcwd();
 
-# to be run after the MixServer has been configured by the make file
+def runMake():
+    os.chdir(tools_path);
+    os.system("make build");
+    os.chdir(current);
+
+# to be run after the 'config_mix[0-9]+.json' files are in the current folder
 def updateKeys():
-    files = os.listdir(path)
+    files = os.listdir(current)
     p=re.compile(pattern) 
     configMix_files=filter(p.search, files);
     for file in configMix_files:
         print "***\t" + file + "\t***\n";
-        subprocess.call(["node", keyGenerator_file, 
-                os.path.join(path, electionManifest), os.path.join(path, file)]);
+        subprocess.call(["node", os.path.join(tools_path,keyGenerator_file), 
+                os.path.join(current,electionManifest), os.path.join(current,file)]);
         print;
 
 if __name__ == '__main__':
+    runMake()
+    print
     updateKeys()
