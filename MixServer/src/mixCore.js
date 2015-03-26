7150d0061cc2f30d093ff79eab865a20088afcf4
escapin <me@escapin.net>
2015-03-26 14:47:36 +0100

    mixCore: execFile used

--- MixServer/src/mixCore.js
+++ MixServer/src/mixCore.js
@@ -2,65 +2,118 @@ var mixCore = function() {
 var exports = {};
 //////////////////////////////////////////////////////////////////////////////////////////////
 
+var child_process = require('child_process');
 
 exports.create = function(encKey, decKey, verifKey, signKey, precServVerifKey, 
 		electionID, numberOfVoters)
 {
 	
-	function processBallots(inputFile_path, outputFile_path) {
-		var spawn = require('child_process').spawn,
-			mixServer = spawn('java', ['-cp', '.:../lib/*',
-			                           'selectvoting.system.wrappers.MixServerWrapperMain',
-			                           encKey, decrKey, verifKey, signKey, precServVerifKey,
-			                           electionID, numberOfVoters, inputFile_path, outputFile_path
-			                           ],
-			                           {cwd: '../../bin/', 	// the working dir of the process: java bin
-										env: process.env});
-		mixServer.on('close', function (code) {
-			if (code !== 0) {
-			    console.log('[mixCore.js] MixWrapper process exited with code ' + code);
-			    process.stdout.write('[mixCore.js] This error code corresponds to:\t');
-			    switch(code){
-			    	case 10:
-			    		console.log('***MixServerWrapper*** \t Wrong Number of Arguments');
-			    		break;
-				    case 11:
-				    	console.log('***MixServerWrapper*** \t [IOException] reading the file');
-				    	break;
-				    case 12:
-				    	console.log('***MixServerWrapper*** \t [IOException] writing the file');
-				    	break;
-				    case 1:
-				    	console.log('***MalformedData*** \t Wrong signature');
-				    	break;
-				    case 2:
-				    	console.log('***MalformedData*** \t Wrong tag');
-				    	break;
-				    case 3:
-				    	console.log('***MalformedData*** \t Wrong election ID');
-				    	break;
-				    case -1:
-				    	console.log('***ServerMisbehavior*** \t Too many entries');
-				    	break;
-				    case -2:
-				    	console.log('***ServerMisbehavior*** \t Ballots not sorted');
-				    	break;
-				    case -3:
-				    	console.log('***ServerMisbehavior*** \t Duplicate ballots');
-				    	break;
-				    	// FIXME: should we do something else if a ServerMisbehavior occurs?
-				    default:
-				    	console.log('***Unknown Errror***');
-			    }
-			    process.exit(code);
-			 }
-			console.log('[mixCore.js] MixWrapper process ended correctly');
-		});
+	function processBallots(inputFile_path, outputFile_path, callbackExec, onClose) {
+//		console.log('\n\n\n\t\t\t' + encKey + '\n\n\n');
+//		var spawn = require('child_process'),
+//		ls    = spawn('ls', ['-lh', '/usr']);
+//		java = spawn('java', ['-version']);
+//		mixServer = spawn('java', 
+//								['-cp', '.:../../lib/*',
+//		                           'selectvoting.system.wrappers.MixServerWrapperMain',
+//		                           encKey, decKey, verifKey, signKey, precServVerifKey ,
+//		                           electionID, numberOfVoters, inputFile_path, outputFile_path
+//		                           ],
+////		                           );
+//		                           {cwd: '../../bin', 	// the working dir of the process: java bin
+//									env: process.env,
+//									stdio: ['pipe', 'pipe', 'pipe']
+//		                           }
+//						);
+//		java.stdout.on('data', function (data){
+//			console.log('stdout: ' + data);
+//		});
+		
+		mixServer = child_process.execFile('java', ['-cp', '.:../lib/*',
+		                'selectvoting.system.wrappers.MixServerWrapperMain',
+		                encKey, decKey, verifKey, signKey, precServVerifKey ,
+		                electionID, numberOfVoters, inputFile_path, outputFile_path
+		                ],
+				        {cwd: '../bin'}, // the working dir of the process: java bin
+				        callbackExec);
+		if(onClose)
+			mixServer.on('close', onClose);
+		
+//		function (err, stdout, stderr){
+//     	   if (err) {
+//console.log("child processes failed with error code: " + err.code);
+//}
+//console.log(stdout);
+//console.log(stderr);
+//}
+	    
+//		ls.stdout.on('data', function (data) {
+//		  console.log('stdout: ' + data);
+//		});
+//
+//		ls.stderr.on('data', function (data) {
+//		  console.log('stderr: ' + data);
+//		});
+//
+//		ls.on('close', function (code) {
+//		  console.log('child process exited with code ' + code);
+//		});
+		
+		
+//		mixServer.stdout.on('data', function (data) {
+//		  console.log('stdout: ' + data);
+//		});
+//		mixServer.on('close', exitCodeHandler);
+//		mixServer.on('close', function (code) {
+//			console.log("Program exited with code " + code);
+//		});
 	}
 	
 	return {encKey : encKey, verifKey : verifKey, precServVerifKey: precServVerifKey,
 			electionID : electionID, numberOfVoters: numberOfVoters,
 			processBallots: processBallots};
+}
+
+
+exports.exitCodeHandler = function exitCodeHandler(code) {
+	if (code !== 0) {
+		console.log('[mixCore.js] MixWrapper process exited with code ' + code);
+	    process.stdout.write('[mixCore.js] This error code corresponds to:\t');
+	    switch(code){
+	    	case 10:
+	    		console.log('***MixServerWrapper*** \t Wrong Number of Arguments');
+	    		break;
+		    case 11:
+		    	console.log('***MixServerWrapper*** \t [IOException] reading the file');
+		    	break;
+		    case 12:
+		    	console.log('***MixServerWrapper*** \t [IOException] writing the file');
+		    	break;
+		    case 1:
+		    	console.log('***MalformedData*** \t Wrong signature');
+		    	break;
+		    case 2:
+		    	console.log('***MalformedData*** \t Wrong tag');
+		    	break;
+		    case 3:
+		    	console.log('***MalformedData*** \t Wrong election ID');
+		    	break;
+		    case -1:
+		    	console.log('***ServerMisbehavior*** \t Too many entries');
+		    	break;
+		    case -2:
+		    	console.log('***ServerMisbehavior*** \t Ballots not sorted');
+		    	break;
+		    case -3:
+		    	console.log('***ServerMisbehavior*** \t Duplicate ballots');
+		    	break;
+		    default:
+		    	console.log('***Unknown Errror***');
+	    }
+	    return code;
+	}
+	console.log('[mixCore.js] MixWrapper process ended correctly');
+	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
