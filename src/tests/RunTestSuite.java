1869054ed0896eaf867a877527abb640017c63ae
escapin <me@escapin.net>
2015-06-08 17:00:14 +0200

    legacy java classes removed

--- src/tests/RunTestSuite.java
+++ src/tests/RunTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-import selectvoting.tests.TestCoreSystem;
 
 @SuppressWarnings("rawtypes")
 public class RunTestSuite {	
@@ -16,7 +15,6 @@ public class RunTestSuite {
 
 		//Add test cases
 		testCases.add(de.unitrier.infsec.tests.TestCryptoLib.class);
-		testCases.add(selectvoting.tests.TestCoreSystem.class);
 		testCases.add(selectvoting.tests.TestMixServer.class);
 
 
