d07357b0db2d11117204b95644c5e5f864d983f1
escapin <me@escapin.net>
2015-10-19 16:26:37 +0200

    cs updated as in dev

--- CollectingServer/src/sendEmail.js
+++ CollectingServer/src/sendEmail.js
@@ -17,7 +17,7 @@ var transporter = nodemailer.createTransport(smtpTransport({
 function sendEmail(address, subject, text, callback) 
 {
     var mailOptions = {
-        from: 'sElect <no-reply@select.uni-trier.de>',
+        from: 'sElect <no-reply@select.org>',
         to: address,
         subject: subject,
         text: text
