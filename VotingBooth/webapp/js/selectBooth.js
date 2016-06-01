2652c93d6183aa319ff2df50fb79a15e4be2ff5a
Enrico Scapin <me@escapin.net>
2016-06-01 16:25:22 +0200

    adjusting size (part II)

--- VotingBooth/webapp/js/selectBooth.js
+++ VotingBooth/webapp/js/selectBooth.js
@@ -43,13 +43,13 @@ function selectBooth() {
     // Create a new canvas element (not attached to the document)
     var verCodeCanvas = document.createElement('canvas');
     verCodeCanvas.setAttribute('width', '480');
-    verCodeCanvas.setAttribute('height', '100');
+    verCodeCanvas.setAttribute('height', '130');
 
     function verificationCode2DataURL(verificationCode, elID, elTitle) {
         // draw the verification code:
         var ctx = verCodeCanvas.getContext('2d');
         ctx.fillStyle = "white";
-        ctx.fillRect(0,0,650,140);
+        ctx.fillRect(0,0,650,130);
         ctx.fillStyle = "black";
         ctx.font = "16px helvetica";
         ctx.fillText('sElect verification code', 10, 25);
