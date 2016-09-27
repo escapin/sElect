adf20a599090ce0b00b41e1bbd8c1e49cacf614d
William Standard <s4wistan@uni-trier.de>
2016-09-27 10:28:05 +0200

    takes seperateAuthentication value from the manifest

--- VotingBooth/webapp/ejs/compileEJS.js
+++ VotingBooth/webapp/ejs/compileEJS.js
@@ -3,18 +3,11 @@ var ejs = require('ejs');
 
 var manifest = JSON.parse(fs.readFileSync("../../ElectionManifest.json"));
 var config = JSON.parse(fs.readFileSync("../../config.json"));
-var seperateAuthentication = process.argv[2];
-if(seperateAuthentication === "True"){
-	seperateAuthentication = true;
-}
-else{
-	seperateAuthentication = false;
-}
 
 var compiled = ejs.compile(fs.readFileSync('votingBooth.ejs', 'utf8'));
-var html = compiled({ seperateAuthentication: seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
+var html = compiled({ seperateAuthentication: manifest.seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
 fs.writeFileSync("../votingBooth.html", html);
 
 compiled = ejs.compile(fs.readFileSync('selectBooth.ejs', 'utf8'));
-html = compiled({ seperateAuthentication: seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
+html = compiled({ seperateAuthentication: manifest.seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
 fs.writeFileSync("../js/selectBooth.js", html);
