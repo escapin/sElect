06d4da2e8ffef1171d46466adfd018d7a76c3391
William Standard <s4wistan@uni-trier.de>
2016-09-28 11:34:30 +0200

    put seperateAuthentication field from manifest to config_vb

--- VotingBooth/webapp/ejs/compileEJS.js
+++ VotingBooth/webapp/ejs/compileEJS.js
@@ -2,13 +2,12 @@ const path = require('path');
 var fs = require('fs');
 var ejs = require('ejs');
 
-var manifest = JSON.parse(fs.readFileSync(path.join(__dirname, "../../ElectionManifest.json")));
 var config = JSON.parse(fs.readFileSync(path.join(__dirname, "../../config.json")));
 
 var compiled = ejs.compile(fs.readFileSync(path.join(__dirname, 'votingBooth.ejs'), 'utf8'));
-var html = compiled({ seperateAuthentication: manifest.seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
+var html = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp });
 fs.writeFileSync(path.join(__dirname, "../votingBooth.html"), html);
 
 compiled = ejs.compile(fs.readFileSync(path.join(__dirname, 'selectBooth.ejs'), 'utf8'));
-html = compiled({ seperateAuthentication: manifest.seperateAuthentication, randomness : manifest.userChosenRandomness, showOtp : config.showOtp });
+html = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp });
 fs.writeFileSync(path.join(__dirname, "../js/selectBooth.js"), html);
