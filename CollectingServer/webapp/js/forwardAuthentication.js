f28b7468203f576294be8fdd0c45cb3800b20954
Enrico Scapin <me@escapin.net>
2016-09-28 12:26:46 +0200

    improving the quality of the client sitde JS code

--- CollectingServer/webapp/js/forwardAuthentication.js
+++ CollectingServer/webapp/js/forwardAuthentication.js
@@ -1,35 +1,34 @@
 function forwardAuthentication(){
 	
-	function callCS(req){
-		var manifest = JSON.parse(sessionStorage.getItem("manifest"));
-        // Make an (ajax) cast request:
-        $.post(manifest.collectingServer.URI+"/cast", req)
-         .fail(function otpRequestFailed() {  // request failed
-            showError('Cannot connect with the server');
-          })
-         .done(function castRequestDone(result) {  // we have some response
-            parent.postMessage({result: result}, "*");
-          });
+	function castBallot(req){
+	    var manifest = JSON.parse(sessionStorage.getItem("manifest"));
+            // Make an (ajax) cast request:
+            $.post(manifest.collectingServer.URI+"/cast", req)
+		.fail(function otpRequestFailed() {  // request failed
+		    showError('Cannot connect with the server');
+		})
+		.done(function castRequestDone(result) {  // the signed acknowledgment
+		    parent.postMessage({result: result}, "*");
+		});
 	}
 	
 	window.addEventListener('message',function(event) {
-		console.log("frame recieved: "+event.data);
-		if(event.data.hasOwnProperty("manifest")){
-			sessionStorage.setItem("manifest", JSON.stringify(event.data.manifest));
-		}
-		else if(event.data === "loaded"){
-			var manifest = JSON.parse(sessionStorage.getItem("manifest"));
-			parent.postMessage({manifest: manifest}, "*");
-		}
-		else if(event.data.hasOwnProperty("submitCS")){
-			sessionStorage.setItem("submitCS", JSON.stringify(event.data.submitCS));
-		}
-		else if(event.data.hasOwnProperty("ballot")){
-			var submit = JSON.parse(sessionStorage.getItem("submitCS"));
-			submit.ballot = event.data.ballot;
-			callCS(submit);
-		}
-		
+	    //console.log("frame recieved: "+event.data);
+	    if(event.data.hasOwnProperty("manifest")){
+		sessionStorage.setItem("manifest", JSON.stringify(event.data.manifest));
+	    }
+	    else if(event.data === "loaded"){
+		var manifest = JSON.parse(sessionStorage.getItem("manifest"));
+		parent.postMessage({manifest: manifest}, "*");
+	    }
+	    else if(event.data.hasOwnProperty("credentials")){
+		sessionStorage.setItem("toBeSubmitted", JSON.stringify(event.data.submitCS));
+	    }
+	    else if(event.data.hasOwnProperty("ballot")){
+		var submit = JSON.parse(sessionStorage.getItem("toBeSubmitted"));
+		submit.ballot = event.data.ballot;
+		castBallot(submit);
+	    }
 	},false);
 
 }
