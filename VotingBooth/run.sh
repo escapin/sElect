54466102bcd6769a4936f52da39ee075644b6404
Enrico Scapin <me@escapin.net>
2016-10-06 16:28:49 +0200

    vb/run.sh generate var configRaw in webapp/config.js

--- VotingBooth/run.sh
+++ VotingBooth/run.sh
@@ -1,3 +1,4 @@
 node ../tools/manifest2js.js ElectionManifest.json > webapp/ElectionManifest.js
+node ../tools/config2js.js config.json configRaw > webapp/config.js
 node ./webapp/ejs/compileEJS.js
 node server.js
