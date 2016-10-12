b0b870740991300ceb7658e1beb630b137fcaf71
William Standard <s4wistan@uni-trier.de>
2016-10-12 04:11:10 +0200

    forcefull approach to enable redirecting in all browsers

--- CollectingServer/webapp/js/authChannel.js
+++ CollectingServer/webapp/js/authChannel.js
@@ -15,25 +15,26 @@ function authChannel(){
 	window.addEventListener('message',function(event) {
 	    //console.log("frame recieved: "+event.data);
 	    if(event.data.hasOwnProperty("manifest")){
-		sessionStorage.setItem("manifest", JSON.stringify(event.data.manifest));
+	    	sessionStorage.setItem("manifest", JSON.stringify(event.data.manifest));
+	    	parent.postMessage("recieved", "*");
 	    }
 	    else if(event.data === "loaded"){
 		// when the parent page is 'loaded', provide to this page the election manifest
 		var manifest = JSON.parse(sessionStorage.getItem("manifest"));
-		parent.postMessage({manifest: manifest}, "*");
+			parent.postMessage({manifest: manifest}, "*");
 	    }
 	    else if(event.data.hasOwnProperty("credentials")){
-		// email and otp
-		sessionStorage.setItem("toBeSubmitted", JSON.stringify(event.data.credentials));
+	    	// email and otp
+	    	sessionStorage.setItem("toBeSubmitted", JSON.stringify(event.data.credentials));
 	    }
 	    else if(event.data.hasOwnProperty("ballot")){
-		// electionID and ballot
-		var submit = JSON.parse(sessionStorage.getItem("toBeSubmitted"));
-		var manifest = JSON.parse(sessionStorage.getItem("manifest"));
-		submit.electionID = manifest.hash;
-		submit.ballot = event.data.ballot;
-		// submit := {email, otp, electionID, ballot}
-		castBallot(submit);
+			// electionID and ballot
+			var submit = JSON.parse(sessionStorage.getItem("toBeSubmitted"));
+			var manifest = JSON.parse(sessionStorage.getItem("manifest"));
+			submit.electionID = manifest.hash;
+			submit.ballot = event.data.ballot;
+			// submit := {email, otp, electionID, ballot}
+			castBallot(submit);
 	    }
 	},false);
 }
