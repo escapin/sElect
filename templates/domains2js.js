f0ec99ef1c2b86c33cb469fe9b02fee69c581ac9
William Standard <s4wistan@uni-trier.de>
2016-10-13 07:40:09 +0200

    directly outputs as js array instead of stringified json

--- templates/domains2js.js
+++ templates/domains2js.js
@@ -4,20 +4,24 @@ var fs = require('fs');
 var selectUtils = require('selectUtils');
 
 var fileName = "trustedDomains.json"
-var varname = "trustedDomainsRaw";
+var varname = "trustedDomains";
 
 var ms = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-var authenticator = JSON.stringify({temp: ms.Authenticator}).replace('{"temp":', "").slice(0, -1);
-var authchannel = JSON.stringify({temp: ms.authChannel}).replace('{"temp":', "").slice(0, -1);
+var authenticator = ms.Authenticator;
+var authchannel = ms.authChannel;
 
-var norm = selectUtils.normalizeManifest(authenticator);
-norm = norm.replace(/\\/g, '\\\\') // Escape backslashes ( \ -> \\ )
-norm = norm.replace(/"/g, '\\"') // Escape Quotes ( " -> \" )
-norm = "var "+varname+" = "+'"'+norm+'"';
-fs.writeFileSync("../Authenticator/webapp/trustedDomain.js", norm);
+var domains = "[";
+for(var i = 0; i < authenticator.length; i++){
+	domains = domains + '"'+authenticator[i]+'",';
+}
+domains = domains.slice(0, -1) + "];";
+domains = "var "+varname+" = "+domains;
+fs.writeFileSync("../Authenticator/webapp/trustedDomains.js", domains);
 
-var norm = selectUtils.normalizeManifest(authchannel);
-norm = norm.replace(/\\/g, '\\\\') // Escape backslashes ( \ -> \\ )
-norm = norm.replace(/"/g, '\\"') // Escape Quotes ( " -> \" )
-norm = "var "+varname+" = "+'"'+norm+'"';
-fs.writeFileSync("../CollectingServer/webapp/trustedDomain.js", norm);
+var domains = "[";
+for(var i = 0; i < authchannel.length; i++){
+	domains = domains + '"'+authchannel[i]+'",';
+}
+domains = domains.slice(0, -1) + "];";
+domains = "var "+varname+" = "+domains;
+fs.writeFileSync("../CollectingServer/webapp/trustedDomains.js", domains);
