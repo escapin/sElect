d394cd94dc8eb591ef517f01cde44240fb38cbac
William Standard <s4wistan@uni-trier.de>
2016-10-17 14:21:05 +0200

    useRadioButtons should compile for selecBooth.js not VotingBooth.html

--- VotingBooth/webapp/ejs/compileEJS.js
+++ VotingBooth/webapp/ejs/compileEJS.js
@@ -6,9 +6,9 @@ var config = JSON.parse(fs.readFileSync(path.join(__dirname, "../../config.json"
 var useRadioButtons = config.maxChoicesPerVoter === 1;
 
 var compiled = ejs.compile(fs.readFileSync(path.join(__dirname, 'votingBooth.ejs'), 'utf8'));
-var html = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp, radioButtons: useRadioButtons});
+var html = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp});
 fs.writeFileSync(path.join(__dirname, "../votingBooth.html"), html);
 
 compiled = ejs.compile(fs.readFileSync(path.join(__dirname, 'selectBooth.ejs'), 'utf8'));
-var js = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp });
+var js = compiled({ seperateAuthentication: config.seperateAuthentication, randomness : config.userChosenRandomness, showOtp : config.showOtp, radioButtons: useRadioButtons});
 fs.writeFileSync(path.join(__dirname, "../js/selectBooth.js"), js);
