541bd08880eea7c1819ac82e2a676a540b0dfee4
William Standard <s4wistan@uni-trier.de>
2016-10-24 18:00:39 +0200

    updated to refresh configs

--- Authenticator/refresh.sh
+++ Authenticator/refresh.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 node ../tools/config2js.js config.json > webapp/config.js configRaw
 node ../tools/config2js.js ElectionManifest.json > webapp/ElectionManifest.js electionManifestRaw
-node server.js
